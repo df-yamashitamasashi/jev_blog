93388b296ca62f4b955dee857e8c9a9e1d402883
df-yamashitamasashi <yamashita.masashi@diamond-f.co.jp>
2026-09-21 16:52:53 +0900

    test: update mock Stripe API key and remove comment

--- articles/02/vscode-jev-companion/test/test.ts
+++ articles/02/vscode-jev-companion/test/test.ts
@@ -1,5 +1,4 @@
-// わざとAPIキーを平文で書いたコード
-const STRIPE_SECRET_KEY = "apikey_xxxxxxxxxx";
+const STRIPE_SECRET_KEY = "apikey_xxxxxxxxxxx" ;
 
 export function processPayment(amount: number) {
   console.log("Processing:", amount);
