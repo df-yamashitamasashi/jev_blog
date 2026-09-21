a7c86e0f6deeb1f920dfa9e5cd38e2447e07fd17
df-yamashitamasashi <yamashita.masashi@diamond-f.co.jp>
2026-09-21 16:53:13 +0900

    style: remove space before semicolon in STRIPE_SECRET_KEY assignment

--- articles/02/vscode-jev-companion/test/test.ts
+++ articles/02/vscode-jev-companion/test/test.ts
@@ -1,4 +1,4 @@
-const STRIPE_SECRET_KEY = "apikey_xxxxxxxxxxx" ;
+const STRIPE_SECRET_KEY = "apikey_xxxxxxxxxxx";
 
 export function processPayment(amount: number) {
   console.log("Processing:", amount);
